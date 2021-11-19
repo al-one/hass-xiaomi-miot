dfbde250e0db08119a4cbc763dd47a81f625c13b
Alone <hi@anlo.ng>
2021-11-19 10:35:59 +0800

    🔧 fix for viomi.washer.u2 (#251)

--- custom_components/xiaomi_miot/switch.py
+++ custom_components/xiaomi_miot/switch.py
@@ -58,7 +58,10 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
                 ENTITY_DOMAIN, 'outlet', 'washer', 'massager', 'towel_rack',
                 'fish_tank', 'pet_drinking_fountain', 'mosquito_dispeller',
             ):
-                if not srv.get_property('on'):
+                if model in ['viomi.washer.u2']:
+                    # issues/251
+                    pass
+                elif not srv.get_property('on'):
                     continue
                 entities.append(MiotSwitchEntity(config, srv))
     for entity in entities:
