3a409368131801dfea0c343997bcb384c05ae09e
Alone <hi@anlo.ng>
2022-02-14 19:12:48 +0800

    💦 add miio2miot for deerma.humidifier.jsq/jsq1/mjjsq (#396)

--- custom_components/xiaomi_miot/core/miio2miot_specs.py
+++ custom_components/xiaomi_miot/core/miio2miot_specs.py
@@ -167,6 +167,31 @@ MIIO_TO_MIOT_SPECS = {
         },
     },
 
+    'deerma.humidifier.jsq': {
+        'chunk_properties': 1,
+        'miio_specs': {
+            'prop.2.1': {'prop': 'OnOff_State', 'setter': 'Set_OnOff', 'set_template': '{{ [value|int] }}'},
+            'prop.3.1': {'prop': 'Humidity_Value'},
+            'prop.3.2': {'prop': 'TemperatureValue'},
+            'prop.4.1': {'prop': 'Led_State', 'setter': 'SetLedState', 'format': '{{ [value|int] }}'},
+            'prop.5.1': {'prop': 'TipSound_State', 'setter': 'SetTipSound_Status', 'format': '{{ [value|int] }}'},
+        },
+    },
+    'deerma.humidifier.jsq1': {
+        'extend_model': 'deerma.humidifier.jsq',
+        'miio_specs': {
+            'prop.2.2': {'prop': 'Humidifier_Gear', 'setter': 'Set_HumidifierGears'},
+            'prop.4.1': {'prop': 'TipSound_State', 'setter': 'SetTipSound_Status', 'format': '{{ [value|int] }}'},
+            'prop.5.1': {'prop': 'Led_State', 'setter': 'SetLedState', 'format': '{{ [value|int] }}'},
+        },
+    },
+    'deerma.humidifier.mjjsq': {
+        'extend_model': 'deerma.humidifier.jsq1',
+        'miio_specs': {
+            'prop.3.3': {'prop': 'HumiSet_Value', 'setter': 'Set_HumiValue'},
+        },
+    },
+
     'fawad.airrtc.fwd20011': {
         'chunk_properties': 1,
         'miio_specs': {
