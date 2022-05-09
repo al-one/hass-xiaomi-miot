30415c450dac26858671e7262be0224e61b32ac7
Alone <hi@anlo.ng>
2022-05-09 14:25:02 +0800

    ♨️ improve for water-heater

--- custom_components/xiaomi_miot/water_heater.py
+++ custom_components/xiaomi_miot/water_heater.py
@@ -86,7 +86,6 @@ class MiotWaterHeaterEntity(MiotToggleEntity, WaterHeaterEntity):
                     await self.async_update_attrs({
                         self._prop_power.full_name: not off,
                     })
-            self._update_sub_entities(self._prop_power.name, domain='switch')
 
     @property
     def state(self):
