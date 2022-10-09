c28bca4c11deca89b1fdb8375f298cf2591100fc
nestor <nestorgu@foxmail.com>
2022-10-09 11:57:31 +0800

    add miio2miot for philips.light.sread2
    only support power and bright

--- custom_components/xiaomi_miot/core/miio2miot_specs.py
+++ custom_components/xiaomi_miot/core/miio2miot_specs.py
@@ -845,6 +845,22 @@ MIIO_TO_MIOT_SPECS = {
             'prop.2.2': {'prop': 'bright', 'setter': True},
         },
     },
+    'philips.light.sread2': {
+        'miio_specs': {
+            'prop.2.1': {'prop': 'power', 'setter': True, 'format': 'onoff'},
+            'prop.2.2': {'prop': 'bright', 'setter': True},
+            'prop.2.3': {'prop': 'scene_num', 'setter': True, 'dict': {
+                'None': 0,
+                'Child Reading': 1,
+                'Adult Reading': 2,
+                'Computer': 3,
+            }},
+            'prop.3.2': {'prop': 'ambstatus', 'setter': True, 'format': 'onoff'},
+            'prop.3.3': {'prop': 'ambvalue', 'setter': True},
+            'prop.3.4': {'prop': 'notifystatus', 'setter': True, 'format': 'onoff'},
+            'prop.3.5': {'prop': 'eyecare', 'setter': True, 'format': 'onoff'},
+        },
+    },
     'philips.light.bulb': {
         'miio_specs': {
             'prop.2.1': {'prop': 'power', 'setter': True, 'format': 'onoff'},
