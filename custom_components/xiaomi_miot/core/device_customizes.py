c419b81538f1b0874746efd440ae1fa727a336a9
José Antonio Yáñez Jiménez <github@codigojose.com>
2024-04-10 17:59:53 +0200

    ⛲ add sub-entities for pet_waterer.s4 (#1615)

--- custom_components/xiaomi_miot/core/device_customizes.py
+++ custom_components/xiaomi_miot/core/device_customizes.py
@@ -990,6 +990,12 @@ DEVICE_CUSTOMIZES = {
         'sensor_properties': 'remain_clean_time,fault,filter_left_time,no_water_time',
         'select_properties': 'mode',
     },
+    'mmgg.pet_waterer.s4': {
+        'binary_sensor_properties': 'no_water_flag,pump_block_flag',
+        'button_actions': 'reset_filter_life,reset_clean_time',
+        'sensor_properties': 'remain_clean_time,fault,filter_left_time,no_water_time',
+        'select_properties': 'mode',
+    },
     'mrbond.airer.m53pro': {
         'position_reverse': False,
         'sensor_properties': 'fault,left_time',
