91654e173989cf94daa9db41c5572ad870e61531
hhh <147622825+hzonz@users.noreply.github.com>
2025-02-10 12:44:59 +0800

    Update device_customizes.py

--- custom_components/xiaomi_miot/core/device_customizes.py
+++ custom_components/xiaomi_miot/core/device_customizes.py
@@ -770,6 +770,20 @@ DEVICE_CUSTOMIZES = {
         ],
         'cover_position_mapping': None
     },
+    'hyd.airer.znlyj5': {
+        'main_miot_services': 'airer',
+        'switch_properties': 'uv',
+        'select_properties': 'mode,dryer',
+        'number_properties': 'drying_time',
+        'exclude_miot_properties': 'motor_control',
+        'disable_target_position': True,
+        'position_reverse': False,
+        'cover_position_mapping': {
+            0: 50,
+            1: 100,
+            2: 0,
+        },
+    },        
     'hyd.airer.*': {
         'switch_properties': 'uv',
         'select_properties': 'mode,dryer',
