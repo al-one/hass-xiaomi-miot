d8862765462cd01479816cc9caae4e3b5f70f69e
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2026-07-31 12:05:50 +0200

    Avoid loading optional component dependencies

--- custom_components/xiaomi_miot/core/const.py
+++ custom_components/xiaomi_miot/core/const.py
@@ -1,9 +1,9 @@
 from enum import StrEnum
 
 from awesomeversion import AwesomeVersion
-from homeassistant.components.alarm_control_panel import AlarmControlPanelState
-from homeassistant.components.camera import CameraState
-from homeassistant.components.vacuum import VacuumActivity
+from homeassistant.components.alarm_control_panel.const import AlarmControlPanelState
+from homeassistant.components.camera.const import CameraState
+from homeassistant.components.vacuum.const import VacuumActivity
 from homeassistant.const import __version__ as HAVERSION  # noqa
 from homeassistant.core import ServiceResponse, SupportsResponse
 from homeassistant.core_config import DATA_CUSTOMIZE
