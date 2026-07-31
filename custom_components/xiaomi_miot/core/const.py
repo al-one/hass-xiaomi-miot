f67bd87e875a3d95c7954ad73dfa4d5c6866bbe1
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2026-07-31 12:12:00 +0200

    Load camera state with camera platform

--- custom_components/xiaomi_miot/core/const.py
+++ custom_components/xiaomi_miot/core/const.py
@@ -2,7 +2,6 @@ from enum import StrEnum
 
 from awesomeversion import AwesomeVersion
 from homeassistant.components.alarm_control_panel.const import AlarmControlPanelState
-from homeassistant.components.camera.const import CameraState
 from homeassistant.components.vacuum.const import VacuumActivity
 from homeassistant.const import __version__ as HAVERSION  # noqa
 from homeassistant.core import ServiceResponse, SupportsResponse
