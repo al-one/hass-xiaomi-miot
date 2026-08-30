f531cf2512054e7ab3bf963d935c1b228946b69c
RanKKI <hliu86.me@gmail.com>
2026-08-30 16:20:54 +1000

    Fix missing MAC address in connection data

--- custom_components/xiaomi_miot/core/device.py
+++ custom_components/xiaomi_miot/core/device.py
@@ -297,12 +297,24 @@ class Device(CustomConfigHelper):
         return {(DOMAIN, self.unique_id)}
 
     @property
+    def connections(self):
+        connections = set()
+
+        if mac := self.info.mac:
+            connections.add(
+                (dr.CONNECTION_NETWORK_MAC, mac)
+            )
+
+        return connections
+
+    @property
     def hass_device_info(self):
         via_device = None
         if self._proxy_device:
             via_device = next(iter(self._proxy_device.identifiers))
         return {
             'identifiers': self.identifiers,
+            'connections': self.connections,
             'name': self.name,
             'model': self.model,
             'manufacturer': (self.model or 'Xiaomi').split('.', 1)[0],
